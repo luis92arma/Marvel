6479dc339224887bb45c10fc553c702030ef811b
luis92arma <luis.arma92@gmail.com>
2016-08-13 19:09:15 -0500

    Este es el bueno

--- hero/wsgi.py
+++ hero/wsgi.py
@@ -1,9 +1,7 @@
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hero.settings")
 
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
