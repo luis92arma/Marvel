8ef70ab033a5248a0c184f03a06759c88a8ee68e
luis92arma <luis.arma92@gmail.com>
2016-08-13 19:40:08 -0500

    static

--- hero/settings.py
+++ hero/settings.py
@@ -130,6 +130,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
