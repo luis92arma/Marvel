e110fa8d68b5078f2f16ca913b2d45b79a8fc0db
luis92arma <luis.arma92@gmail.com>
2016-08-13 19:26:25 -0500

    heroku

--- hero/settings.py
+++ hero/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 #Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
